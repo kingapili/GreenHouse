64d9dfc5e6914be58d11a20ef9b4f1c583e117bb
Bartek <kuskowski.bartosz@gmail.com>
2021-11-28 19:15:35 +0100

    GUI

---
+++ GUI/Models/FilterTableForm.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GUI.Models
+{
+    public class FilterForm
+    {
+        public int sensorId { get; set; }
+        public string sensorType { get; set; }
+        [DataType(DataType.DateTime), Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{yyyy-MM-ddTHH:mm:ss.sssZ}")]
+        public DateTime dateTime { get; set; }
+        
+
+
+    }
+}
---
+++ GUI/Models/GraphForm.cs
@@ -0,0 +1,7 @@
+namespace GUI.Models
+{
+    public class GraphForm
+    {
+        
+    }
+}
